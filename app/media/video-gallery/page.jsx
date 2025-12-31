6853c53acc08d161f304fe466e1b598dd004c911
Shvm0032 <shvmbutola@gmail.com>
2025-12-31 13:08:22 +0530

    image gallery

---
+++ app/media/video-gallery/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+import { PageHero } from "../../../components/page-hero";
+
+
+const page = () => {
+ 
+
+  return (
+    <>
+      <PageHero
+        title="Video Gallery"
+        subtitle="Moments from Campus Life"
+        description="Explore our university campus through images showcasing academics, infrastructure, events, and student life."
+        image="/news/news13.jpg"
+      />
+
+      {/*--------------------------------------------- Section 2 --------------------------------------------------*/}
+
+      
+    </>
+  );
+};
+
+export default page;
