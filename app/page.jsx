8b093411aef5a69d4a684612933f1312805a597d
developmentrankmantra-sudo <developmentrankmantra@gmail.com>
2026-01-03 16:13:20 +0530

    bsc programme done

--- app/page.jsx
+++ app/page.jsx
@@ -45,7 +45,7 @@ export default function HomePage() {
       <Highlights/>
       <Programs/>
       <CTA/>
-      <NewsSection/>
+      {/* <NewsSection/> */}
       <SchoolHighlights/>
       <AccreditationsMarquee/>
       <StudentLife/>
